631d8fb2511df76021432f449ddfe4d2c64978b0
rolando22 <rolando2207@gmail.com>
2023-09-11 15:36:22 -0300

    [ADD] cálculo de total, ingresos y gastos

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,21 @@
+import { useTransactionsContext } from "../../hooks/useTransactionsContext";
+
 export function Header() {
+    const { total, incomes, expense } = useTransactionsContext();
+
     return (
         <header className='grid gap-2 bg-neutral-800 p-10 rounded-md w-full'>
             <section className=' flex justify-between items-center'>
                 <h4>Ingresos</h4>
-                <p>$10000</p>
+                <p>${incomes}</p>
             </section>
             <section className=' flex justify-between items-center'>
                 <h4>Gastos</h4>
-                <p>$5000</p>
+                <p>${expense * -1}</p>
             </section>
             <section className=' flex justify-between items-center'>
                 <h4 className='text-slate-400'>Tu Balance</h4>
-                <h1 className='text-2xl font-bold'>$5000</h1>
+                <h1 className='text-2xl font-bold'>${total}</h1>
             </section>
         </header>
     );
--- src/context/transactions.tsx
+++ src/context/transactions.tsx
@@ -4,6 +4,9 @@ import type { Transaction, TransactionWithId } from "../types/transaction";
 
 interface ContextProps {
     transactions: TransactionWithId[]
+    total: number 
+    incomes: number 
+    expense: number
     addTransaction: (transaction: Transaction) => void
     removeTransaction: ({ id }: { id: string }) => void
 }
@@ -20,9 +23,21 @@ export function TransactionsProvider({ children }: Props) {
     const addTransaction = (transaction: Transaction) => dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
     const removeTransaction = ({ id }: { id: string }) => dispatch({ type: 'REMOVE_TRANSACTION', payload: id });
 
+    const [total, incomes, expense] = state
+        //.filter(transaction => transaction.amount > 0)
+        .reduce((accumulador, transaction) => {
+            accumulador[0] += transaction.amount;
+            transaction.amount > 0 ? accumulador[1] += transaction.amount : accumulador[2] += transaction.amount;
+            return accumulador;
+        }, [0, 0, 0])
+        // .map(calc => calc.toFixed(2));
+
     return (
         <TransactionsContext.Provider value={{
             transactions: state,
+            total, 
+            incomes, 
+            expense,
             addTransaction,
             removeTransaction, 
         }}>
